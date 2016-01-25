f120c303e59f3d12afa289e594cbe39abf167f70
Sharundaar <rabiller.donatien@hotmail.fr>
2016-01-25 18:44:36 -0500

    Initial Commit.

---
+++ Assets/Editor/TextureEffects.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TextureEffects : EditorWindow {
+
+    Texture2D m_source;
+    Texture2D m_destination;
+    string m_path = "";
+
+    [MenuItem("Window/Texture Effects Editor")]
+    static void Init()
+    {
+        TextureEffects window = EditorWindow.GetWindow<TextureEffects>();
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Greyscale to Alpha", EditorStyles.boldLabel);
+        m_path = GUILayout.TextField(m_path);
+        EditorGUILayout.BeginHorizontal();
+        m_source = EditorGUILayout.ObjectField(m_source, typeof(Texture2D), false) as Texture2D;
+        m_destination = EditorGUILayout.ObjectField(m_destination, typeof(Texture2D), false) as Texture2D;
+
+        if(GUILayout.Button("Apply"))
+        {
+            ComputeGreyScaleToAlphaEffect();
+        }
+
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void ComputeGreyScaleToAlphaEffect()
+    {
+        var pathSplit = m_path.Split('/');
+        var fileName = pathSplit[pathSplit.Length - 1];
+        string path = "";
+        foreach(var s in pathSplit)
+        {
+            path += s == fileName ? "" : s + "/";
+        }
+
+        if (m_source == null || m_destination == null || m_source.width != m_destination.width || m_source.height != m_destination.height)
+            return;
+
+        if (!System.IO.Directory.Exists(FileUtil.GetProjectRelativePath("") + "Assets/" + path))
+            return;
+
+        Texture2D result = new Texture2D(m_source.width, m_source.height);
+        Color[] colors = new Color[m_source.width * m_source.height];
+        Color[] src = m_source.GetPixels();
+        Color[] dest = m_destination.GetPixels();
+
+        for(int i=0; i<colors.Length; ++i)
+        {
+            colors[i] = new Color(src[i].r, src[i].g, src[i].b, dest[i].r);
+        }
+
+        result.SetPixels(colors);
+        result.Apply();
+
+        result.name = "AlphaToGreyScaleResult";
+
+        string completePath = FileUtil.GetProjectRelativePath("") + "Assets/" + path + fileName;
+        byte[] png = result.EncodeToPNG();
+        System.IO.File.WriteAllBytes(completePath, png);
+    }
+}
---
+++ Assets/Scripts/AutoSinMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSinMovement : MonoBehaviour {
+
+    [SerializeField]
+    float m_amplitude = 1.0f;
+
+    [SerializeField]
+    float m_frequency = 1.0f;
+
+    [SerializeField]
+    Vector3 m_direction = Vector3.right;
+
+    Vector3 m_initialPosition;
+
+	// Use this for initialization
+	void Start () {
+        m_initialPosition = transform.position;
+        m_direction = m_direction.normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = m_initialPosition + Mathf.Sin(Time.time * m_frequency * (Mathf.PI * 2.0f)) * m_direction * m_amplitude;
+	}
+}
