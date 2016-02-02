0739c58e50217abac8d077a87f7ee1cd9dfc4095
bourgeoismaxime <bourgeois.maxime@gmail.com>
2016-02-02 18:27:36 -0500

    Started the base of the interaction system (WIP)
    - Interfaces + abstract class
    - Basic use of the raycast to find an interactive object => display a
    message to notice the player that an interactive object have been found

---
+++ Assets/Scripts/Interaction/IActivable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Interface Activable : used for objects which can be activated */
+interface IActivable 
+{
+	bool OnActivate(Object callingObject); 
+	bool IsAuthorized(Object callingObject);
+}
