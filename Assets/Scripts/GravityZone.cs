b3e85ad1d867ef0722f382daa3928cbb04ad6a09
Sharundaar <rabiller.donatien@hotmail.fr>
2016-02-03 14:37:36 -0500

    Test on the multiplayer. Test on Gravity. Added a simple room prefab

---
+++ Assets/Scripts/GravityZone.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityZone : MonoBehaviour {
+
+    [SerializeField]
+    private Vector3 m_GravityDirection;
+
+	void OnTriggerEnter(Collider _collider)
+    {
+        GravityFirstPersonController controller = _collider.GetComponent<GravityFirstPersonController>();
+        if(controller != null)
+        {
+            controller.SetGravity(Physics.gravity.magnitude * m_GravityDirection);
+        }
+    }
+
+    void OnTriggerExit(Collider _collider)
+    {
+        GravityFirstPersonController controller = _collider.GetComponent<GravityFirstPersonController>();
+        if (controller != null)
+        {
+            controller.SetGravity(Physics.gravity);
+        }
+    }
+}
---
+++ Assets/Scripts/NetworkCommands.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public class NetworkCommands : NetworkBehaviour {
+
+    private Dictionary<NetworkInstanceId, NetworkIdentity> m_objects = new Dictionary<NetworkInstanceId, NetworkIdentity>(); 
+
+	// Use this for initialization
+	void Start () {
+        var sceneObjects = FindObjectsOfType<NetworkIdentity>();
+        foreach(var obj in sceneObjects)
+        {
+            m_objects.Add(obj.netId, obj);
+        }
+	}
+
+    [ClientRpc]
+    public void RpcRegisterNetworkId(NetworkInstanceId id)
+    {
+        var sceneObjects = FindObjectsOfType<NetworkIdentity>();
+        foreach (var obj in sceneObjects)
+        {
+            if(obj.netId.Equals(id))
+            {
+                m_objects.Add(id, obj);
+                break;
+            }
+        }
+    }
+
+    [ClientRpc]
+    public void RpcSetParentTransformToNull(GameObject child)
+    {
+        // var childId = m_objects[child];
+        var childTransform = child.GetComponent<Transform>();
+
+        childTransform.parent = null;
+    } 
+
+    [ClientRpc]
+    public void RpcSetParentTransform(GameObject child, GameObject parent)
+    {
+        // var childId = m_objects[child];
+
+        var childTransform = child.GetComponent<Transform>();
+
+        // var parentId = m_objects[parent];
+        var parentTransform = parent.GetComponent<Transform>();
+
+        childTransform.parent = parentTransform;
+    }
+
+    [ClientRpc]
+    public void RpcSetUseGravity(GameObject obj, bool value)
+    {
+        // var objId = m_objects[obj];
+        var objRigidbody = obj.GetComponent<Rigidbody>();
+        objRigidbody.useGravity = value;
+    }
+}
